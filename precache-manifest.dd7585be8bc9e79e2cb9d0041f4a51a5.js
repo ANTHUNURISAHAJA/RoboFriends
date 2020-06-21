19a27d7870e563c63f0e9967fbd789c4be04a7a6
sahaja <sahaja@gmail.com>
2020-06-21 22:49:59 +0530

    Updates

---
+++ precache-manifest.dd7585be8bc9e79e2cb9d0041f4a51a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc98b0834a05b35ed2c9831f513da46e",
+    "url": "/RoboFriends/index.html"
+  },
+  {
+    "revision": "25c787df19f3d36d9d78",
+    "url": "/RoboFriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "60852e3e8eb58b183277",
+    "url": "/RoboFriends/static/css/main.71e4b8e2.chunk.css"
+  },
+  {
+    "revision": "25c787df19f3d36d9d78",
+    "url": "/RoboFriends/static/js/2.b7cedf7a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RoboFriends/static/js/2.b7cedf7a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60852e3e8eb58b183277",
+    "url": "/RoboFriends/static/js/main.9790a8f4.chunk.js"
+  },
+  {
+    "revision": "f3a4dbb7b6d5d91c7195",
+    "url": "/RoboFriends/static/js/runtime-main.aa93969a.js"
+  }
+]);
